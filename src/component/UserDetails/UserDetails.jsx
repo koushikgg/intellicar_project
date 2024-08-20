e74ec4884fe9ab6d4d924a67a2ecb5eee3a802c2
koushikgg <iamkoushikg@gmail.com>
2024-08-20 14:53:56 +0530

    [KOUSHIK]ADDED: fixed some css

--- src/component/UserDetails/UserDetails.jsx
+++ src/component/UserDetails/UserDetails.jsx
@@ -3,7 +3,7 @@ import "./UserDetails.scss"
 import { getUserInfoApi } from "../../service/userService";
 
 function UserDetails() {
-    const [user , setUser] = useState({})
+    const [user, setUser] = useState({})
     useEffect(() => {
         getUserDetails()
     }, [])
@@ -12,20 +12,20 @@ function UserDetails() {
             const res = await getUserInfoApi();
             console.log(res.data.data);
             setUser(res.data.data)
-            
+
         } catch (error) {
             console.log(error);
             alert(error.response.data.error)
         }
-        
+
     }
     return (
         <>
             <h1 id="userdetails-txt">User Details:</h1>
             <div className="userDetails-main-cnt">
-                <div class Name="userDetails-profile-cnt">
+                <div className="userDetails-profile-cnt">
                     <div className="userDetails-profile-inner-cnt">
-                        <p>P</p>
+                        <p>{user?.username ? user.username[0] : 'P'}</p>
                     </div>
                 </div>
                 <div className="userDetails-pass-cnt">
@@ -51,7 +51,7 @@ function UserDetails() {
                 <div className="userDetails-pass-cnt">
                     <span id="userDetails-name-txt">Phone Number  </span>
                     <div className="userDetails-text-cnt">
-                        <p>: {user?.mobile }</p>
+                        <p>: {user?.mobile}</p>
                     </div>
                 </div>
                 <div className="userDetails-btn-cnt">
